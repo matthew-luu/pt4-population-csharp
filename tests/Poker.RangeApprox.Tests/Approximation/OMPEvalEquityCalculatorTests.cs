4eb0b2cd1dfa471f21e12c22707201ac0a146bce
matthew-luu <mtluu2@yahoo.com>
2026-03-08 16:44:05 -0400

    Everything is working

--- tests/Poker.RangeApprox.Tests/Approximation/OMPEvalEquityCalculatorTests.cs
+++ tests/Poker.RangeApprox.Tests/Approximation/OMPEvalEquityCalculatorTests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using Poker.RangeApprox.Core.Domain;
+using Poker.RangeApprox.Core.Equity;
 using Poker.RangeApprox.Equity.OMPEval;
 
 namespace Poker.RangeApprox.Tests.Approximation;
@@ -8,20 +9,41 @@ namespace Poker.RangeApprox.Tests.Approximation;
 public class OMPEvalEquityCalculatorTests
 {
     [Test]
-    public void CalculateEquity_CallsNativeStubSuccessfully()
+    public void CalculateEquity_ComputesRealEquity_ForSimpleHeadsUpCase()
     {
-        var calculator = new OMPEvalEquityCalculator(iterations: 1000);
+        var calculator = new OMPEvalEquityCalculator(iterations: 100000);
 
         var villainRange = new List<RangeCell>
         {
-            new(HandClass.ParseRankingToken("AKo"), 1.0),
-            new(HandClass.ParseRankingToken("AQs"), 0.5)
+            new(HandClass.ParseRankingToken("AKo"), 1.0)
         };
 
         var equity = calculator.CalculateEquity(
             HandClass.ParseRankingToken("QQp"),
             villainRange);
 
-        Assert.That(equity, Is.EqualTo(0.5).Within(0.000001));
+        Assert.That(equity, Is.GreaterThan(0.50));
+        Assert.That(equity, Is.LessThan(0.60));
+    }
+
+    [Test]
+    public void RankAllHands_ReturnsSortedEquities()
+    {
+        var calculator = new OMPEvalEquityCalculator(iterations: 50000);
+        var rankingService = new HandVsRangeRankingService(calculator);
+
+        var villainRange = new List<RangeCell>
+        {
+        new(HandClass.ParseRankingToken("AKo"), 1.0)
+        };
+
+        var ranking = rankingService.RankAllHands(villainRange);
+
+        Assert.That(ranking.Count, Is.EqualTo(169));
+
+        Assert.That(
+            ranking.Zip(ranking.Skip(1))
+                   .All(pair => pair.First.Equity >= pair.Second.Equity),
+            Is.True);
     }
 }
